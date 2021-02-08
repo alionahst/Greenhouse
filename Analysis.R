b9512ee8ebdf45b31faf14f1605df775a5dbcfc0
alionahst <aliona.96h@gmail.com>
2021-02-08 13:43:00 +0100

    21/02/08 13:42

--- Analysis.R
+++ Analysis.R
@@ -1,3 +1,6 @@
 #creating data.frame with the experiment results
-Data_csv <- "210129_Greenhouse_results.csv"
-Data <- read.table(Data_csv, header = TRUE, sep = ";")
+#Data_csv <- "210129_Greenhouse_results.csv"
+#Data <- read.table(Data_csv, header = TRUE, sep = ";")
+
+library("readxl")
+Data <- read_excel("210129_Greenhouse_results.xlsx")

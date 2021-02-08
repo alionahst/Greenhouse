88b145dc476d3bd349737a2ed18dd05ccae655c0
alionahst <aliona.96h@gmail.com>
2021-02-08 17:11:44 +0100

    21/02/08 17:11

--- Parameters_evolution_graph.R
+++ Parameters_evolution_graph.R
@@ -12,6 +12,8 @@ Data$Date2 <- as.Date(Data$Date2, format='%d/%m/%y')
 
 library(ggplot2)
 
+
+
 # Leaf number
 #ggplot(Data[Data$Species =="Avena",], aes(x=Date2, y = Leaf_number, color = Treatment, na.rm=TRUE )) + geom_point() + geom_smooth()
 #ggplot(Data[Data$Species =="Avena",], aes(x=Date2, y = Leaf_number, color = Treatment, na.rm=TRUE )) + geom_jitter() + geom_smooth()
@@ -25,25 +27,23 @@ ggplot(Data, aes(x=Date2, y = Leaf_number, color = Treatment,  linetype = Specie
 
 
 # Stem_Length
-ggplot(Data[Data$Species =="Avena",], aes(x=Date2, y = Stem_length, color = Treatment, na.rm=TRUE )) + geom_smooth()  + labs(title="Evolution of stem length in Avena Fatua", y="Stem length", x="Date") + theme(plot.title = element_text(hjust = 0.5))
-ggplot(Data[Data$Species =="Triticum",], aes(x=Date2, y = Stem_length, color = Treatment, na.rm=TRUE )) + geom_smooth() + labs(title="Evolution of stem length in Triticum aestivum", y="Stem length", x="Date") + theme(plot.title = element_text(hjust = 0.5))
+ggplot(Data[Data$Species =="Avena",], aes(x=Date2, y = Stem_length, color = Treatment, na.rm=TRUE )) + geom_smooth()  + labs(title="Evolution of stem length in Avena Fatua", y="Stem length (cm)", x="Date") + theme(plot.title = element_text(hjust = 0.5))
+ggplot(Data[Data$Species =="Triticum",], aes(x=Date2, y = Stem_length, color = Treatment, na.rm=TRUE )) + geom_smooth() + labs(title="Evolution of stem length in Triticum aestivum", y="Stem length (cm)", x="Date") + theme(plot.title = element_text(hjust = 0.5))
 
-ggplot(Data, aes(x=Date2, y = Stem_length, color = Treatment,  linetype = Species, na.rm=TRUE )) + geom_smooth(se=F) + labs(title="Comparison of stem length evolution between Avena Fatua and Triticum aestivum", y="stem length", x="Date") + theme(plot.title = element_text(hjust = 0.5)) 
+ggplot(Data, aes(x=Date2, y = Stem_length, color = Treatment,  linetype = Species, na.rm=TRUE )) + geom_smooth(se=F) + labs(title="Comparison of stem length evolution between Avena Fatua and Triticum aestivum", y="stem length (cm)", x="Date") + theme(plot.title = element_text(hjust = 0.5)) 
 
 
 # Relative humidity
-ggplot(Data[Data$Species =="Avena",], aes(x=Date2, y = RH, color = Treatment, na.rm=TRUE )) + geom_smooth()  + labs(title="Evolution of relative humidity in Avena Fatua", y="Relative humidity", x="Date") + theme(plot.title = element_text(hjust = 0.5))
+ggplot(Data[Data$Species =="Avena",], aes(x=Date2, y = RH, color = Treatment, na.rm=TRUE )) + geom_smooth()  + labs(title="Evolution of relative humidity in Avena Fatua", y="Relative humidity (%)", x="Date") + theme(plot.title = element_text(hjust = 0.5))
 # Test statistical difference between 100mM and control for RH. 
-ggplot(Data[Data$Species =="Triticum",], aes(x=Date2, y = RH, color = Treatment, na.rm=TRUE )) + geom_smooth() + labs(title="Evolution of relative humidity in Triticum Aestivum", y="Relative humidity", x="Date") + theme(plot.title = element_text(hjust = 0.5))
+ggplot(Data[Data$Species =="Triticum",], aes(x=Date2, y = RH, color = Treatment, na.rm=TRUE )) + geom_smooth() + labs(title="Evolution of relative humidity in Triticum Aestivum", y="Relative humidity (%)", x="Date") + theme(plot.title = element_text(hjust = 0.5))
 
-ggplot(Data, aes(x=Date2, y = Leaf_number, color = Treatment,  linetype = Species, na.rm=TRUE )) + geom_smooth(se=F) + labs(title="Comparison of relative humidity evolution between Avena Fatua and Triticum aestivum", y="Relative humidity", x="Date") + theme(plot.title = element_text(hjust = 0.5))
+ggplot(Data, aes(x=Date2, y = Leaf_number, color = Treatment,  linetype = Species, na.rm=TRUE )) + geom_smooth(se=F) + labs(title="Comparison of relative humidity evolution between Avena Fatua and Triticum aestivum", y="Relative humidity (%)", x="Date") + theme(plot.title = element_text(hjust = 0.5))
 
 
 # EC
-ggplot(Data[Data$Species =="Avena",], aes(x=Date2, y = EC, color = Treatment, na.rm=TRUE )) + geom_smooth() + labs(title="Evolution of EC number in Avena Fatua", y="EC", x="Date") + theme(plot.title = element_text(hjust = 0.5))
-ggplot(Data[Data$Species =="Triticum",], aes(x=Date2, y = EC, color = Treatment, na.rm=TRUE )) + geom_smooth() + labs(title="Evolution of EC number in Triticum aestivum", y="EC", x="Date") + theme(plot.title = element_text(hjust = 0.5))
+ggplot(Data[Data$Species =="Avena",], aes(x=Date2, y = EC, color = Treatment, na.rm=TRUE )) + geom_smooth() + labs(title="Evolution of EC number in Avena Fatua", y="EC (mS/cm)", x="Date") + theme(plot.title = element_text(hjust = 0.5))
+ggplot(Data[Data$Species =="Triticum",], aes(x=Date2, y = EC, color = Treatment, na.rm=TRUE )) + geom_smooth() + labs(title="Evolution of EC number in Triticum aestivum", y="EC(mS/cm)", x="Date") + theme(plot.title = element_text(hjust = 0.5))
 
-ggplot(Data, aes(x=Date2, y = EC, color = Treatment,  linetype = Species, na.rm=TRUE )) + geom_smooth(se=F) + labs(title="Comparison of EC evolution between Avena Fatua and Triticum aestivum", y="EC", x="Date") + theme(plot.title = element_text(hjust = 0.5))
-
-
+ggplot(Data, aes(x=Date2, y = EC, color = Treatment,  linetype = Species, na.rm=TRUE )) + geom_smooth(se=F) + labs(title="Comparison of EC evolution between Avena Fatua and Triticum aestivum", y="EC (mS/cm)", x="Date") + theme(plot.title = element_text(hjust = 0.5))
 
